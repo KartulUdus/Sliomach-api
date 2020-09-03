c156b76fef72fe6bbce4e63755d4a3424acb336f
KartulUdus <derek.maekask@gmail.com>
2020-09-03 21:36:50 +0300

    testing stuff

--- src/routes/timelapse/getLatest.js
+++ src/routes/timelapse/getLatest.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = async (fastify, options, next) => {
     fastify.get('/timelapse/latest', options, async (req, reply) => {
 
-		const imageFiles = await fastify.readDirRecursive(path.resolve(__dirname, '../../../timelapse/'))
+		const imageFiles = fs.readdirSync(path.resolve(__dirname, '../../../timelapse/'))
 		console.log(imageFiles)
         
     })
