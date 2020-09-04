260bea6787c3e3baa7e4351d8bbdd08d2e6a69b2
KartulUdus <derek.maekask@gmail.com>
2020-09-04 14:55:00 +0300

    reply with latest measurements

--- src/routes/readings/getLatest.js
+++ src/routes/readings/getLatest.js
@@ -2,12 +2,8 @@ const fs = require('fs')
 const path = require('path')
 module.exports = async (fastify, options, next) => {
     fastify.get('/readings/latest', options, async (req, reply) => {
-
 		const result = await fastify.db('measurements').select().limit(1).orderBy('id', 'desc')
-		console.log(result)
-		console.log(result[0])
-		return result[0] ? result[0] : result
-        
+		return result[0] ? result[0] : result       
     })
     next()
 }
