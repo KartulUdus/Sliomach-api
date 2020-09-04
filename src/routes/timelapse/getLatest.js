19e58345d3521425733c68de71960f4001aaaa92
KartulUdus <derek.maekask@gmail.com>
2020-09-04 14:00:26 +0300

    reply with latest timelapse image

--- src/routes/timelapse/getLatest.js
+++ src/routes/timelapse/getLatest.js
@@ -4,7 +4,18 @@ module.exports = async (fastify, options, next) => {
     fastify.get('/timelapse/latest', options, async (req, reply) => {
 
 		const imageFiles = fs.readdirSync(path.resolve(__dirname, '../../../timelapse/'))
-		console.log(imageFiles)
+		let latest = 0
+
+		for (const imageFile of imageFiles) {
+			if(imageFile.endsWith('.png') && +imageFile.replace('.png', '') && +imageFile.replace('.png', '') > latest) {
+				latest = +imageFile.replace('.png', '')
+			}
+			
+		}
+		console.log(latest)
+		const buffer = fs.readFileSync(path.resolve(__dirname, '../../../timelapse/', `${latest}.png`))
+		reply.type('image/png') 
+  		reply.send(buffer)
         
     })
     next()
